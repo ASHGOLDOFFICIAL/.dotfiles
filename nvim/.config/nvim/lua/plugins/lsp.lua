d983ec6e632ea02ba6a2884d3e315d4ef0a5a251
Andrey Shaat <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2024-06-09 03:19:10 +0500

    nvim: remove cmp-buffer and cmp-calc plugins

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -107,9 +107,7 @@ return {
                 build = "make install_jsregexp",
             },
             "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
-            "hrsh7th/cmp-calc",
         },
         opts = function()
             return require("configs.cmp")
