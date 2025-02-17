d108f1bb5ac8ede233902ee6d8d8e3d6eaf29d3a
ashgoldofficial <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2025-02-17 23:23:27 +0500

    nixos: skyscraper update

--- nixfiles/packages/skyscraper.nix
+++ nixfiles/packages/skyscraper.nix
@@ -1,19 +1,26 @@
-{ lib
-, stdenv
-, fetchFromGitHub
-, qt5
-, p7zip
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  qt5,
+  p7zip,
+  python3,
+  installShellFiles,
+
+  # Whether to compile with XDG support
+  # (See: https://gemba.github.io/skyscraper/XDG/)
+  enableXdg ? false,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "skyscraper";
-  version = "3.12.0";
+  version = "3.15.3";
 
   src = fetchFromGitHub {
     owner = "Gemba";
     repo = "skyscraper";
     rev = "refs/tags/${finalAttrs.version}";
-    hash = "sha256-KmRow3Cw34Y/UBFEmjUSPMqb4O49DAcfCMu7qVSrtKw=";
+    hash = "sha256-UgPo9qEsYYYl2RgdPtPmToAruPKuvJ3FSgVrXa94kdA=";
   };
 
   strictDeps = true;
@@ -21,9 +28,24 @@ stdenv.mkDerivation (finalAttrs: {
   nativeBuildInputs = [
     qt5.wrapQtAppsHook
     qt5.qmake
+    installShellFiles
   ];
 
-  path = lib.makeBinPath [ p7zip ];
+  buildInputs = [ python3 ];
+
+  postPatch = lib.optionalString enableXdg ''
+    substituteInPlace skyscraper.pro --replace-fail "#DEFINES+=XDG" "DEFINES+=XDG"
+  '';
+
+  postInstall = ''
+    installShellCompletion --cmd Skyscraper \
+      --bash supplementary/bash-completion/Skyscraper.bash
+  '';
+
+  preFixup = ''
+    qtWrapperArgs+=(--prefix PATH : ${lib.makeBinPath [ p7zip ]})
+    chmod +x $out/bin/*.py
+  '';
 
   env.PREFIX = placeholder "out";
 
@@ -31,7 +53,7 @@ stdenv.mkDerivation (finalAttrs: {
     description = "Powerful and versatile game data scraper written in Qt and C++";
     homepage = "https://gemba.github.io/skyscraper/";
     downloadPage = "https://github.com/Gemba/skyscraper/releases/tag/${finalAttrs.version}";
-    license = lib.licenses.gpl3Only;
+    license = lib.licenses.gpl3Plus;
     maintainers = with lib.maintainers; [ ashgoldofficial ];
     mainProgram = "Skyscraper";
     platforms = lib.platforms.linux;
